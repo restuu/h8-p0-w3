d799e4c61dba24999e8e3b011658c3097f0d5c92
restu <restu_u@outlook.com>
2018-03-06 10:02:39 +0700

    rocker live code 1 completed

--- live-code/rocket.js
+++ live-code/rocket.js
@@ -1,15 +1,26 @@
 function drawSymbolicColsBox(height) {
-  for (var i = 1; i <= height; i++) {
-    var symbol = '';
-    for (var j = 1; j <= height; j++) {
-      if (j % 2 !== 0) {
+  var symbol = '';
+  for (var i = 1; i <= height*height; i++) {
+    if ((i % 2 !== 0 && i % 3 === 0) || (i % 2 === 0 && i % 3 === 0)) {
+        symbol += '*';
+      } else if (i % 2 !== 0) {
         symbol += '@';
-      } else if (j % 2 === 0) {
+      } else if (i % 2 === 0) {
         symbol += '$';
-      } else if (j % 3 === 0) {
-        symbol += '*'
       }
+  }
+  var rowSymbol = '';
+  for (var j = 1; j <= symbol.length; j++){
+    if (j % height === 0){
+      rowSymbol += symbol[j-1]+'\n';
+    } else {
+      rowSymbol += symbol[j-1]+' ';
     }
   }
-  console.log(symbol);
+  console.log (rowSymbol);
 }
+
+
+drawSymbolicColsBox(3);
+drawSymbolicColsBox(5);
+drawSymbolicColsBox(1);
