65ba497685b652d9399e9eb0adb195f26555faa4
restu <restu_u@outlook.com>
2018-03-22 12:52:17 +0700

    add note

--- JUMAT/exercise15-hewan.js
+++ JUMAT/exercise15-hewan.js
@@ -1,11 +1,11 @@
 function groupAnimals(animals) {
   animals.sort();
   var group = [[animals[0]]];     //there must be an array within array,
-  // console.log(animals);        //because it'll be grouped within an array
-  // console.log(group);
+                                  //because it'll be grouped within an array
+
   var element = 0;                // to declare which index array within group[] to put into
-  for (var i = 1; i < animals.length; i++) {
-    if (animals[i - 1].charAt(0) !== animals[i].charAt(0)) {
+  for (var i = 1; i < animals.length; i++) {    //if I make it (length - 1) the last element won't be included in the loop
+    if (animals[i - 1].charAt(0) !== animals[i].charAt(0)) {    //check with before not after because it's not (length - 1)
       element++;
       group[element] = [];        //if next element is diff.,
     }                             //then close the current index array then creat new array in front
